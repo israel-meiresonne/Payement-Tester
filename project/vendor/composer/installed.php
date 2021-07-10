ee4972b162b013d881abae9bf156b06730c77bff
Israel Meiresonne <israelmeiresonne97@gmail.com>
2021-07-10 10:00:04 +0200

    install Stripe's library

---
+++ project/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9a880ee2a0597344d0a580572d9f73664360672b',
+        'name' => 'project/fuel',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'project/fuel' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9a880ee2a0597344d0a580572d9f73664360672b',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.87.0',
+            'version' => '7.87.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '9392f03cb8d8803bf8273378ce42d5cbbf1e24fc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
